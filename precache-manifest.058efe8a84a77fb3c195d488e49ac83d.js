6eaf4116156bd58eac400881b30bf1c60dbb740a
Eira <irinabelova.nz@gmail.com>
2019-10-18 17:13:03 +1300

    Updates

---
+++ precache-manifest.058efe8a84a77fb3c195d488e49ac83d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdce8860b44a1272f7471431e38244f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5a8fffa8508dad23cfc5",
+    "url": "/static/css/main.33575798.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "5a8fffa8508dad23cfc5",
+    "url": "/static/js/main.a5b1935e.chunk.js"
+  },
+  {
+    "revision": "05efcc9d811b4a2a3c6e",
+    "url": "/static/js/runtime-main.8f8b62bf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
